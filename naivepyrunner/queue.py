9f8cdf01d696b97b512b36af3b8df6bb58bb0b97
Henning Janßen <development@henning-janssen.net>
2018-05-30 03:31:34 +0200

    DuetimeQueue: use heapq instead of dequeue

--- naivepyrunner/queue.py
+++ naivepyrunner/queue.py
@@ -1,6 +1,7 @@
 from collections import deque
 from threading import Lock
 from time import time
+import heapq
 
 class Queue(object):
     def __init__(self, *args, **kwargs):
@@ -28,54 +29,27 @@ class Queue(object):
                 return self.queue.popleft()
         return None
 
-class DuetimeQueue(Queue):
+class DuetimeQueue(Queue):#
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.queue = []
+
     def insert(self, job):
         with self.lock:
-            queue_len = len(self.queue)
-            now = time()
+            heapq.heappush(self.queue, (job.duetime, job))
 
-            if len(self.queue) == 0:
-                self.queue.append(job)
+    def pop(self):
+        with self.lock:
+            try:
+                return heapq.heappop(self.queue)[1]
+            except IndexError:
+                return None
 
-            # naive approach to find the shorter side to start from
-            elif (queue_len == 1
-                or abs(self.queue[0].duetime - now) >
-                    abs(self.queue[-1].duetime - now)
-            ):
-                self._insert_from_left(job)
-
-            else:
-                self._insert_from_right(job)
-
-    def _insert_from_left(self, job):
-        if self.queue[0].duetime > job.duetime:
-            self.queue.appendleft(job)
-
-        inserted = False
-        for i in range(0, len(self.queue)-1):
-            if (self.queue[i].duetime <= job.duetime
-                    and self.queue[i+1].duetime > job.duetime):
-                self.queue.insert(i+1, job)
-                inserted = True
-                break
-
-        if not inserted:
-            self.queue.append(job)
-
-    def _insert_from_right(self, job):
-        if self.queue[-1].duetime < job.duetime:
-            self.queue.append(job)
-
-        inserted = False
-        for i in range(len(self.queue)-1, 0, -1):
-            if (self.queue[i].duetime > job.duetime
-                    and self.queue[i-1].duetime <= job.duetime):
-                self.queue.insert(i, job)
-                inserted = True
-                break
-
-        if not inserted:
-            self.queue.appendleft(job)
+    def pop_if_due(self):
+        with self.lock:
+            if self.queue and self.queue[0][1].is_due():
+                return heapq.heappop(self.queue)[1]
+            return None
 
 class TimeboxQueue(Queue):
     pass
